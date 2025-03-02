69359e80ef173e4c351680b8897c3efa07fb9701
redapy <baha.redaaaa@gmail.com>
2025-03-02 14:56:56 +0000

    simulate asynch operations to fetch db

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,6 +1,7 @@
 const express = require("express");
 let books = require("./booksdb.js");
 let { users, isValid } = require("./auth_users.js");
+const { default: axios } = require("axios");
 
 const public_users = express.Router();
 
@@ -26,51 +27,95 @@ public_users.post("/register", (req, res) => {
   }
 });
 
+public_users.get("/books", (req, res) => {
+  return res.status(200).json(books);
+});
+
 // Get the book list available in the shop
-public_users.get("/", function (req, res) {
-  const formatedBooks = JSON.stringify(books, null, 4);
-  return res.status(300).send(formatedBooks);
+public_users.get("/", async function (req, res) {
+  try {
+    const booksData = await axios.get("http://localhost:3000/books");
+    return res.status(200).send(booksData.data);
+  } catch (e) {
+    return res.status(500).json({ message: e.message });
+  }
 });
 
 // Get book details based on ISBN
-public_users.get("/isbn/:isbn", function (req, res) {
+public_users.get("/isbn/:isbn", async function (req, res) {
   const isbn = req.params.isbn;
-  const book = books[isbn];
 
-  if (book) {
-    return res.status(300).send(book);
-  } else {
-    return res.status(404).json({ message: "Could not find the book" });
+  if (!isbn) {
+    return res.status(400).json({ message: "ISBN is required" });
+  }
+
+  try {
+    const booksData = await axios.get("http://localhost:3000/books");
+    const book = booksData.data?.[isbn];
+
+    if (book) {
+      return res.status(300).send(book);
+    } else {
+      return res.status(404).json({ message: "Could not find the book" });
+    }
+  } catch (e) {
+    return res.status(500).json({ message: e.message });
   }
 });
 
 // Get book details based on author
-public_users.get("/author/:author", function (req, res) {
+public_users.get("/author/:author", async function (req, res) {
   const author = req.params.author;
-  const booksArray = Object.values(books);
-  const book = booksArray.find(
-    (book) => normalizeStrings(book.author) === normalizeStrings(author)
-  );
 
-  if (book) {
-    return res.status(300).send(book);
-  } else {
-    return res.status(404).json({ message: "Could not find the book" });
+  if (!author) {
+    return res.status(400).json({ message: "Author is required" });
+  }
+
+  try {
+    const booksData = await axios.get("http://localhost:3000/books");
+    const booksArray = Object.values(booksData.data);
+
+    const filteredBooks = booksArray.filter(
+      (book) => normalizeStrings(book.author) === normalizeStrings(author)
+    );
+
+    if (filteredBooks.length > 0) {
+      return res.status(200).json(filteredBooks);
+    } else {
+      return res
+        .status(404)
+        .json({ message: "Could not find books by this author" });
+    }
+  } catch (e) {
+    return res.status(500).json({ message: e.message });
   }
 });
 
 // Get all books based on title
-public_users.get("/title/:title", function (req, res) {
+public_users.get("/title/:title", async function (req, res) {
   const title = req.params.title;
-  const booksArray = Object.values(books);
-  const book = booksArray.find(
-    (book) => normalizeStrings(book.title) === normalizeStrings(title)
-  );
 
-  if (book) {
-    return res.status(300).send(book);
-  } else {
-    return res.status(404).json({ message: "Could not find the book" });
+  if (!title) {
+    return res.status(400).json({ message: "Title is required" });
+  }
+
+  try {
+    const booksData = await axios.get("http://localhost:3000/books");
+    const booksArray = Object.values(booksData.data);
+
+    const filteredBooks = booksArray.filter(
+      (book) => normalizeStrings(book.title) === normalizeStrings(title)
+    );
+
+    if (filteredBooks.length > 0) {
+      return res.status(200).json(filteredBooks);
+    } else {
+      return res
+        .status(404)
+        .json({ message: "Could not find books with this title" });
+    }
+  } catch (e) {
+    return res.status(500).json({ message: e.message });
   }
 });
 
